faa5369c55459db2cb76c5e01b952743423e87f0
Shadas <shadas.zhao@gmail.com>
2018-03-02 17:15:46 +0800

    Add 19 Remove Nth Node From End of List.

---
+++ algorithms/_19_Remove_Nth_Node_From_End_of_List/answer.go
@@ -0,0 +1,28 @@
+package _19_Remove_Nth_Node_From_End_of_List
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	pre := &ListNode{Next: head}
+	ret, fast, slow := pre, pre, pre
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return ret.Next
+}

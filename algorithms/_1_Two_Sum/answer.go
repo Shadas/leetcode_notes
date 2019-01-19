cd6a9c5e327a9860327ba972d94ce0b9f042edb4
Shadas <shadas.zhao@gmail.com>
2019-01-19 14:23:45 +0800

    _1_Two_Sum answer refine.

--- algorithms/_1_Two_Sum/answer.go
+++ algorithms/_1_Two_Sum/answer.go
@@ -2,10 +2,13 @@ package _1_Two_Sum
 
 func twoSum(nums []int, target int) []int {
 	ret := []int{}
-	for i1, n1 := range nums {
-		for i2, n2 := range nums {
-			if i1 != i2 && (n1+n2) == target {
-				ret = []int{i1, i2}
+	for k1, v1 := range nums {
+		for k2, v2 := range nums {
+			if k1 >= k2 {
+				continue
+			}
+			if v1+v2 == target {
+				ret = append(ret, k1, k2)
 				return ret
 			}
 		}

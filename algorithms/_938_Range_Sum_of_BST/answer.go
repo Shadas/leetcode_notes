99c6bd20e495181dc7b95f7a4af34457d8646ca6
Shadas <shadas.zhao@gmail.com>
2019-07-12 19:28:06 +0800

    938 增加非递归解法

--- algorithms/_938_Range_Sum_of_BST/answer.go
+++ algorithms/_938_Range_Sum_of_BST/answer.go
@@ -9,6 +9,39 @@ package _938_Range_Sum_of_BST
  * }
  */
 func rangeSumBST(root *TreeNode, L int, R int) int {
+	// return rangeSumBSTUnrecursion(root, L, R)
+	return rangeSumBSTRecursion(root, L, R)
+}
+
+func rangeSumBSTUnrecursion(root *TreeNode, L int, R int) int {
+	var (
+		l   []*TreeNode = []*TreeNode{}
+		sum int
+	)
+	l = append(l, root)
+	for len(l) > 0 {
+		x := l[len(l)-1]
+		l = l[:len(l)-1]
+		if x == nil {
+			continue
+		}
+		if x.Val < L {
+			if x.Right != nil {
+				l = append(l, x.Right)
+			}
+		} else if x.Val > R {
+			if x.Left != nil {
+				l = append(l, x.Left)
+			}
+		} else {
+			sum += x.Val
+			l = append(l, x.Left, x.Right)
+		}
+	}
+	return sum
+}
+
+func rangeSumBSTRecursion(root *TreeNode, L int, R int) int {
 	var sum int
 	if root.Val >= L && root.Left != nil {
 		sum += rangeSumBST(root.Left, L, R)

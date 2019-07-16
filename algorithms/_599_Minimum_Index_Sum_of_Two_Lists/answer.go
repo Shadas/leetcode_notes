08aa1f9f9736574dc428ca9e02f7c6a377e33c6a
Shadas <shadas.zhao@gmail.com>
2019-07-16 12:03:23 +0800

    Refine 599 loop detail

--- algorithms/_599_Minimum_Index_Sum_of_Two_Lists/answer.go
+++ algorithms/_599_Minimum_Index_Sum_of_Two_Lists/answer.go
@@ -10,22 +10,27 @@ func findRestaurantLoopSearch(list1 []string, list2 []string) []string {
 		minIdx int = len(list1) + len(list2)
 	)
 	for k1, v1 := range list1 {
+		if k1 > minIdx {
+			break
+		}
 		for k2, v2 := range list2 {
+			if k2 > minIdx {
+				break
+			}
 			if v1 == v2 {
 				sum := k1 + k2
+				if sum > minIdx {
+					break
+				}
 				if l, ok := m[sum]; ok {
 					l = append(l, v2)
 					m[sum] = l
 				} else {
 					m[sum] = []string{v2}
 				}
+				minIdx = sum
 				break
 			}
-		}
-	}
-	for idx, _ := range m {
-		if idx < minIdx {
-			minIdx = idx
 		}
 	}
 	return m[minIdx]

beb122c19dad58305b098adb00435d1f31c477d4
Shadas <shadas.zhao@gmail.com>
2019-07-19 15:54:39 +0800

    Add 153 Find Minimum in Rotated Sorted Array

---
+++ algorithms/_153_Find_Minimum_in_Rotated_Sorted_Array/answer.go
@@ -0,0 +1,19 @@
+package _153_Find_Minimum_in_Rotated_Sorted_Array
+
+func findMin(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	var curr int = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > curr {
+			curr = nums[i]
+		} else {
+			return nums[i]
+		}
+	}
+	return nums[0]
+}

9a451980aa765229c7361d19ca52952b7bd29536
Shadas <shadas.zhao@gmail.com>
2019-12-06 18:59:15 +0800

    Refine 1's answer, add hash solution

--- algorithms/_1_Two_Sum/answer.go
+++ algorithms/_1_Two_Sum/answer.go
@@ -1,6 +1,26 @@
 package _1_Two_Sum
 
 func twoSum(nums []int, target int) []int {
+	// return twoSumForce(nums, target)
+	return twoSumHash(nums, target)
+}
+
+func twoSumHash(nums []int, target int) []int {
+	var (
+		m = make(map[int]int)
+	)
+	for idx, n := range nums {
+		m[n] = idx
+	}
+	for idx1, n := range nums {
+		if idx2, ok := m[target-n]; ok && idx1 != idx2 {
+			return []int{idx1, idx2}
+		}
+	}
+	return []int{}
+}
+
+func twoSumForce(nums []int, target int) []int {
 	ret := []int{}
 	for k1, v1 := range nums {
 		for k2, v2 := range nums {

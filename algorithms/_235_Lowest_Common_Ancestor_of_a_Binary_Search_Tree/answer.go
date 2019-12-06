3329013f3bdbc41762d42d96aeb42da10d660f1a
Shadas <shadas.zhao@gmail.com>
2019-12-06 16:27:10 +0800

    Add 235 Lowest Common Ancestor of a Binary Search Tree

---
+++ algorithms/_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/answer.go
@@ -0,0 +1,29 @@
+package _235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}

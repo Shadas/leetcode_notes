f75cf8949a8e534f470df1f4cb3fc38b56b719b1
Shadas <shadas.zhao@gmail.com>
2021-01-27 11:58:46 +0800

    refine 875

--- algorithms/_875_Koko_Eating_Bananas/answer.go
+++ algorithms/_875_Koko_Eating_Bananas/answer.go
@@ -1,23 +1,22 @@
 package _875_Koko_Eating_Bananas
 
+import "math"
+
 func minEatingSpeed(piles []int, H int) int {
 	var (
-		min, max, mid int
+		min = 1
+		max = math.MaxInt32
+		mid int
 	)
-	min = 1
-	max = maxn(piles)
-	if min == max {
-		return min
-	}
-	for min < max {
-		mid = (min + max) / 2
+	for mid != (min+max)>>1 {
+		mid = (min + max) >> 1
 		if eatOver(piles, mid, H) {
 			max = mid
 		} else {
 			min = mid + 1
 		}
 	}
-	return min
+	return mid
 }
 
 func eatOver(piles []int, k, H int) bool {
@@ -28,19 +27,9 @@ func eatOver(piles []int, k, H int) bool {
 		} else {
 			total += pile/k + 1
 		}
-	}
-	if total <= H {
-		return true
-	} else {
-		return false
-	}
-}
-
-func maxn(nums []int) (max int) {
-	for _, n := range nums {
-		if n > max {
-			max = n
+		if total > H {
+			return false
 		}
 	}
-	return
+	return total <= H
 }

23a8cf1f894679b80f46da84eaab4fa92038c1ea
Shadas <shadas.zhao@gmail.com>
2021-08-13 18:45:27 +0800

    17 append dfs

--- algorithms/_17_Letter_Combinations_of_a_Phone_Number/answer.go
+++ algorithms/_17_Letter_Combinations_of_a_Phone_Number/answer.go
@@ -1,11 +1,6 @@
 package _17_Letter_Combinations_of_a_Phone_Number
 
 func letterCombinations(digits string) []string {
-
-	return letterCombinationsLoop(digits)
-}
-
-func letterCombinationsLoop(digits string) []string {
 	dict := map[string][]string{
 		"2": {"a", "b", "c"},
 		"3": {"d", "e", "f"},
@@ -16,6 +11,32 @@ func letterCombinationsLoop(digits string) []string {
 		"8": {"t", "u", "v"},
 		"9": {"w", "x", "y", "z"},
 	}
+	//return letterCombinationsLoop(dict, digits)
+	return letterCombinationsDfs(dict, digits)
+}
+
+func letterCombinationsDfs(dict map[string][]string, digits string) []string {
+	if digits == "" {
+		return []string{}
+	}
+	ret := []string{}
+	dfs(dict, &ret, 0, "", digits)
+	return ret
+}
+
+func dfs(dict map[string][]string, ret *[]string, idx int, tmp, digits string) {
+	if idx == len(digits) {
+		*ret = append(*ret, tmp)
+		return
+	}
+	for _, v := range dict[string(digits[idx])] {
+		tmp += v
+		dfs(dict, ret, idx+1, tmp, digits)
+		tmp = tmp[0 : len(tmp)-1]
+	}
+}
+
+func letterCombinationsLoop(dict map[string][]string, digits string) []string {
 	ret := []string{}
 	for _, v := range digits {
 		ret = foo(dict, ret, string(v))

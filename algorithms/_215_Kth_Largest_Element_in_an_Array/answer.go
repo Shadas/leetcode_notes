716eb9b7caa5acf5cd52b4969964df37ebbdda7f
Shadas <shadas.zhao@gmail.com>
2021-09-26 11:32:41 +0800

    Add 215 Kth Largest Element in an Array with Partion Sorting

--- algorithms/_215_Kth_Largest_Element_in_an_Array/answer.go
+++ algorithms/_215_Kth_Largest_Element_in_an_Array/answer.go
@@ -8,7 +8,37 @@ import (
 func findKthLargest(nums []int, k int) int {
 	//return findKthLargestWithKLengthArray(nums, k)
 	//return findKthLargestAfterSortingAllElements(nums, k)
-	return findKthLargestWithHeap(nums, k)
+	//return findKthLargestWithHeap(nums, k)
+	return findKthLargestWithPartion(nums, k)
+}
+
+// 快排partion思路，持续找一个中间值，当这个中间值左边的数为k-1时，这个数即为第k大的数，不需要全排序
+func findKthLargestWithPartion(nums []int, k int) int {
+	low, high := 0, len(nums)
+	for low < high {
+		i, j := low, high-1
+		pivot := nums[low] // 取low位置的值
+		for i <= j {
+			for i <= j && nums[i] >= pivot {
+				i++
+			}
+			for i <= j && nums[j] <= pivot {
+				j--
+			}
+			if i < j {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+		nums[low], nums[j] = nums[j], nums[low]
+		if j == k-1 {
+			return nums[j]
+		} else if j < k-1 {
+			low = j + 1
+		} else {
+			high = j
+		}
+	}
+	return 0
 }
 
 // 优先队列/堆
@@ -42,7 +72,7 @@ func (h *IntHeap) Pop() interface{} {
 	return x
 }
 
-// 全排序然后取第K位
+// 全排序然后取第K位，数据量大的时候效率不好，因为不需要关注所有元素的排序
 func findKthLargestAfterSortingAllElements(nums []int, k int) int {
 	sort.Ints(nums) // 升序
 	return nums[len(nums)-k]

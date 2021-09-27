156a8b1030e18d922bfdc81a6fd8e293f23b2e83
Shadas <shadas.zhao@gmail.com>
2021-09-27 14:57:38 +0800

    Add util

--- utils/array/strarray.go
+++ utils/array/strarray.go
@@ -1,1 +1,14 @@
 package array
+
+// IsStrArrayEqual compares two []string and returns if they are the same.
+func IsStrArrayEqual(a1, a2 []string) bool {
+	if len(a1) != len(a2) {
+		return false
+	}
+	for i := 0; i < len(a1); i++ {
+		if a1[i] != a2[i] {
+			return false
+		}
+	}
+	return true
+}

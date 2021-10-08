7a402cc869238b38de9d983e44d910259ef0394e
Shadas <shadas.zhao@gmail.com>
2021-10-08 11:35:21 +0800

    513 Add BFS solution

--- algorithms/_513_Find_Bottom_Left_Tree_Value/answer.go
+++ algorithms/_513_Find_Bottom_Left_Tree_Value/answer.go
@@ -7,9 +7,42 @@ type TreeNode struct {
 }
 
 func findBottomLeftValue(root *TreeNode) int {
-	return findBottomLeftValueDFS(root)
+	//return findBottomLeftValueDFS(root)
+	return findBottomLeftValueBFS(root)
 }
 
+// BFS 解法
+func findBottomLeftValueBFS(root *TreeNode) int {
+	if root == nil {
+		return -1
+	}
+	var (
+		level, tmp []*TreeNode
+		first      = true
+		ret        int
+	)
+	level = append(level, root)
+	for len(level) != 0 {
+		first = true
+		for _, node := range level {
+			if first {
+				ret = node.Val
+				first = false
+			}
+			if node.Left != nil {
+				tmp = append(tmp, node.Left)
+			}
+			if node.Right != nil {
+				tmp = append(tmp, node.Right)
+			}
+		}
+		level = tmp
+		tmp = []*TreeNode{}
+	}
+	return ret
+}
+
+// DFS 解法
 func findBottomLeftValueDFS(root *TreeNode) int {
 	ret, _ := DFS(root, 0, 0)
 	return ret

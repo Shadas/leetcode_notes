4dc7072ee47859ae8336a69895e59537b34377bb
Shadas <shadas.zhao@gmail.com>
2021-10-09 11:38:29 +0800

    297 增加后续遍历方式

--- algorithms/_297_Serialize_and_Deserialize_Binary_Tree/answer.go
+++ algorithms/_297_Serialize_and_Deserialize_Binary_Tree/answer.go
@@ -20,13 +20,47 @@ func Constructor() Codec {
 
 // Serializes a tree to a single string.
 func (this *Codec) serialize(root *TreeNode) string {
-	return preOrderSerialize(root)
+	return postOrderSerialize(root)
+	//return preOrderSerialize(root)
 }
 
 // Deserializes your encoded data to tree.
 func (this *Codec) deserialize(data string) *TreeNode {
-	startCur := 0
-	return preOrderDeserialize(data, &startCur)
+	//startCur := 0
+	//return preOrderDeserialize(data, &startCur)
+	startCur := len(data) - 1
+	return postOrderDeserialize(data, &startCur)
+}
+
+// 后续遍历序列化
+func postOrderSerialize(root *TreeNode) string {
+	var str string
+	if root == nil {
+		str += ","
+		return str
+	}
+	str += postOrderSerialize(root.Left)
+	str += postOrderSerialize(root.Right)
+	str += fmt.Sprintf(",%d", root.Val)
+	return str
+}
+
+// 后序遍历反序列化
+func postOrderDeserialize(data string, cur *int) *TreeNode {
+	if string(data[*cur]) == "," {
+		*cur -= 1
+		return nil
+	}
+	begin := *cur
+	for begin >= 0 && string(data[begin]) != "," {
+		begin--
+	}
+	num, _ := strconv.ParseInt(data[begin+1:*cur+1], 10, 64)
+	node := &TreeNode{Val: int(num)}
+	*cur = begin - 1
+	node.Right = postOrderDeserialize(data, cur)
+	node.Left = postOrderDeserialize(data, cur)
+	return node
 }
 
 // 先序遍历序列化
@@ -42,6 +76,7 @@ func preOrderSerialize(root *TreeNode) string {
 	return str
 }
 
+// 先序遍历反序列化
 func preOrderDeserialize(data string, cur *int) *TreeNode {
 	if string(data[*cur]) == "," { // 说明是空节点
 		*cur += 1
@@ -58,6 +93,30 @@ func preOrderDeserialize(data string, cur *int) *TreeNode {
 	node.Right = preOrderDeserialize(data, cur)
 	return node
 }
+
+//// 层序遍历序列化
+//func bfsSerialize(root *TreeNode) string {
+//	var (
+//		str   string
+//		level = []*TreeNode{root}
+//	)
+//	for len(level) != 0 {
+//		node := level[0]
+//		newStr := ""
+//		if node == nil {
+//			newStr = fmt.Sprintf("%d,", node.Val)
+//		} else {
+//			newStr = fmt.Sprintf("#,")
+//		}
+//		str += newStr
+//	}
+//	return str
+//}
+//
+//// 层序遍历反序列化
+//func bfsDeserialize(root *TreeNode) string {
+//
+//}
 
 /**
  * Your Codec object will be instantiated and called as such:
--- algorithms/_297_Serialize_and_Deserialize_Binary_Tree/answer_test.go
+++ algorithms/_297_Serialize_and_Deserialize_Binary_Tree/answer_test.go
@@ -24,3 +24,23 @@ func TestPreOrderSerialize(t *testing.T) {
 	root = preOrderDeserialize(str, &start)
 	fmt.Println(root)
 }
+
+func TestPostOrderSerialize(t *testing.T) {
+	var (
+		root  *TreeNode
+		str   string
+		start int
+	)
+	root = &TreeNode{Val: 1, Left: nil, Right: &TreeNode{Val: 2}}
+	str = postOrderSerialize(root)
+	fmt.Println(str)
+	start = len(str) - 1
+	root = postOrderDeserialize(str, &start)
+	fmt.Println(root)
+	root = nil
+	str = postOrderSerialize(root)
+	fmt.Println(str)
+	start = len(str) - 1
+	root = postOrderDeserialize(str, &start)
+	fmt.Println(root)
+}

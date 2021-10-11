93534d35818a350f6d1c5481aba78c5057efc2b6
Shadas <shadas.zhao@gmail.com>
2021-10-11 18:22:30 +0800

    Add 300 with greedy

--- algorithms/_300_Longest_Increasing_Subsequence/answer.go
+++ algorithms/_300_Longest_Increasing_Subsequence/answer.go
@@ -1,7 +1,8 @@
 package _300_Longest_Increasing_Subsequence
 
 func lengthOfLIS(nums []int) int {
-	return lengthOfLISDP(nums)
+	//return lengthOfLISDP(nums)
+	return lengthOfLISGreedy(nums)
 }
 
 func maxInt(a, b int) int {
@@ -11,6 +12,7 @@ func maxInt(a, b int) int {
 	return b
 }
 
+// dp解法
 func lengthOfLISDP(nums []int) int {
 	if len(nums) == 0 {
 		return 0
@@ -31,4 +33,40 @@ func lengthOfLISDP(nums []int) int {
 		max = maxInt(max, dp[i])
 	}
 	return max
+}
+
+// 贪心法构造具体数列解法
+func lengthOfLISGreedy(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	var (
+		lis = []int{nums[0]}
+	)
+	for i := 1; i < len(nums); i++ {
+		val := nums[i]
+		head, tail := lis[0], lis[len(lis)-1]
+		if val <= head {
+			lis[0] = val
+		} else if val > tail {
+			lis = append(lis, val)
+		} else {
+			lis[findPos(lis, val)] = val
+		}
+	}
+	return len(lis)
+}
+
+// 二分法找第一个>=val的位置
+func findPos(lis []int, val int) int {
+	l, r := 0, len(lis)-1
+	for l < r {
+		mid := l + (r-l)/2
+		if lis[mid] >= val {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
 }
--- algorithms/_300_Longest_Increasing_Subsequence/answer_test.go
+++ algorithms/_300_Longest_Increasing_Subsequence/answer_test.go
@@ -7,3 +7,15 @@ func TestLengthOfLIS(t *testing.T) {
 		t.Errorf("should be 4, wrong length with %d", ret)
 	}
 }
+
+func TestFindPos(t *testing.T) {
+	if pos := findPos([]int{1, 2, 3}, 1); pos != 0 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+	if pos := findPos([]int{1, 2, 3}, 2); pos != 1 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+	if pos := findPos([]int{1, 2, 4}, 3); pos != 2 {
+		t.Errorf("wrong pos with %d", pos)
+	}
+}

8c0300dc9b1a5e732d433e95ddad628c49932be7
Shadas <shadas.zhao@gmail.com>
2021-10-18 14:31:50 +0800

    Add 589 N-ary Tree Preorder Traversal

--- algorithms/_589_N-ary_Tree_Preorder_Traversal/answer.go
+++ algorithms/_589_N-ary_Tree_Preorder_Traversal/answer.go
@@ -1,1 +1,23 @@
 package _589_N_ary_Tree_Preorder_Traversal
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func preorder(root *Node) []int {
+	return preOrderR(root)
+}
+
+// 递归解法
+func preOrderR(root *Node) []int {
+	var ret []int
+	if root == nil {
+		return ret
+	}
+	ret = append(ret, root.Val)
+	for _, n := range root.Children {
+		ret = append(ret, preOrderR(n)...)
+	}
+	return ret
+}

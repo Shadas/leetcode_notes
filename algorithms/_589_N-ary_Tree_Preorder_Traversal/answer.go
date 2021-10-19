3674ef96fe372c24522b5ce972ca0b90816a229f
Shadas <shadas.zhao@gmail.com>
2021-10-19 10:45:48 +0800

    Add 589 迭代解法

--- algorithms/_589_N-ary_Tree_Preorder_Traversal/answer.go
+++ algorithms/_589_N-ary_Tree_Preorder_Traversal/answer.go
@@ -6,7 +6,8 @@ type Node struct {
 }
 
 func preorder(root *Node) []int {
-	return preOrderR(root)
+	//return preOrderR(root)
+	return preOrderI(root)
 }
 
 // 递归解法
@@ -18,6 +19,26 @@ func preOrderR(root *Node) []int {
 	ret = append(ret, root.Val)
 	for _, n := range root.Children {
 		ret = append(ret, preOrderR(n)...)
+	}
+	return ret
+}
+
+// 迭代解法
+func preOrderI(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	var (
+		ret   []int
+		stack = []*Node{root}
+	)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[0 : len(stack)-1]
+		ret = append(ret, node.Val)
+		for i := len(node.Children) - 1; i >= 0; i-- {
+			stack = append(stack, node.Children[i])
+		}
 	}
 	return ret
 }

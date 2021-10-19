01e04c7a16cd062eba12f7a7336266422a2ab459
Shadas <shadas.zhao@gmail.com>
2021-10-19 17:04:26 +0800

    Add 232 Implement Queue using Stacks

---
+++ algorithms/_232_Implement_Queue_using_Stacks/answer.go
@@ -0,0 +1,58 @@
+package _232_Implement_Queue_using_Stacks
+
+type MyQueue struct {
+	in, out []int // 两个栈，分别处理进出
+	top     int
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	q := MyQueue{}
+	return q
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	if len(this.in) == 0 {
+		this.top = x
+	}
+	this.in = append(this.in, x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+	if len(this.out) == 0 {
+		for len(this.in) > 0 {
+			// pop
+			x := this.in[len(this.in)-1]
+			this.in = this.in[:len(this.in)-1]
+			// push
+			this.out = append(this.out, x)
+		}
+	}
+	ret := this.out[len(this.out)-1]
+	this.out = this.out[:len(this.out)-1]
+	return ret
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	if len(this.out) == 0 {
+		return this.top
+	}
+	return this.out[len(this.out)-1]
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	return len(this.in) == 0 && len(this.out) == 0
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */

f8497276bc0895acc6969b9aeff199e7eab1e714
Shadas <shadas.zhao@gmail.com>
2021-10-21 18:44:25 +0800

    Add 162 Find Peak Element

---
+++ algorithms/_162_Find_Peak_Element/answer.go
@@ -0,0 +1,23 @@
+package _162_Find_Peak_Element
+
+func findPeakElement(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	if len(nums) == 1 {
+		return 0
+	}
+	// 边界
+	if nums[0] > nums[1] {
+		return 0
+	}
+	if nums[len(nums)-1] > nums[len(nums)-2] {
+		return len(nums) - 1
+	}
+	for i := 1; i < len(nums)-1; i++ {
+		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
+			return i
+		}
+	}
+	return -1
+}

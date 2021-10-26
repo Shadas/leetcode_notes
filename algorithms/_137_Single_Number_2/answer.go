d1d49d46f9d64b566b238a62fcc1241bf3d932aa
Shadas <shadas.zhao@gmail.com>
2021-10-26 15:16:49 +0800

    Add 137 Single Number 2

--- algorithms/_137_Single_Number_2/answer.go
+++ algorithms/_137_Single_Number_2/answer.go
@@ -3,6 +3,28 @@ package _137_Single_Number_2
 const Times = 3
 
 func singleNumber(nums []int) int {
+	//return singleNumberWithCalcu(nums)
+	return singleNumberWithBit(nums)
+}
+
+// 位运算解法
+func singleNumberWithBit(nums []int) int {
+	a, b := 0, 0
+	for _, num := range nums {
+		a = (a ^ num) & ^b
+		b = (^a ^ num) ^ b
+	}
+	return a
+}
+
+// 算术求和解法，先去重，再*3，减去所有原来num和，即2倍的目标值。此处不予实现。
+func singleNumberWithSum(nums []int) int {
+	// ...
+	return 0
+}
+
+// 计数解法
+func singleNumberWithCalcu(nums []int) int {
 	mark := []bool{}
 	for i := 0; i < len(nums); i++ {
 		mark = append(mark, false)

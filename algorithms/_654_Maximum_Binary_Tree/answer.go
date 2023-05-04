69645a8526371c3cd399be5b10be05355d983269
Shadas <shadas.zhao@gmail.com>
2023-05-04 15:03:20 +0800

    Add 654 Maximum Binary Tree with Stack solution

--- algorithms/_654_Maximum_Binary_Tree/answer.go
+++ algorithms/_654_Maximum_Binary_Tree/answer.go
@@ -7,7 +7,8 @@ type TreeNode struct {
 }
 
 func constructMaximumBinaryTree(nums []int) *TreeNode {
-	return constructMaximumBinaryTreeRecursively(nums)
+	//return constructMaximumBinaryTreeRecursively(nums)
+	return constructMaximumBinaryTreeStack(nums)
 }
 
 func constructMaximumBinaryTreeRecursively(nums []int) *TreeNode {
@@ -27,4 +28,35 @@ func constructMaximumBinaryTreeRecursively(nums []int) *TreeNode {
 	x.Left = constructMaximumBinaryTreeRecursively(nums[0:maxIdx])
 	x.Right = constructMaximumBinaryTreeRecursively(nums[maxIdx+1:])
 	return x
+}
+
+func constructMaximumBinaryTreeStack(nums []int) *TreeNode {
+	var (
+		treeNodePos = make([]*TreeNode, len(nums))
+		stack       []int // stack of num index
+		maxPosIdx   = 0
+	)
+	for idx, num := range nums {
+		currNode := &TreeNode{
+			Val: num,
+		}
+		treeNodePos[idx] = currNode
+		for len(stack) != 0 {
+			if nums[stack[len(stack)-1]] < num {
+				currNode.Left = treeNodePos[stack[len(stack)-1]]
+				// pop
+				stack = stack[0 : len(stack)-1]
+			} else {
+				topIdx := stack[len(stack)-1]
+				treeNodePos[topIdx].Right = currNode
+				stack = append(stack, idx)
+				break
+			}
+		}
+		if len(stack) == 0 {
+			stack = append(stack, idx)
+			maxPosIdx = idx
+		}
+	}
+	return treeNodePos[maxPosIdx]
 }
